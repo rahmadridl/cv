0e2ff0a54f965c9c1299db579206c4224a9658fb
unknown <rahmadridho17@gmail.com>
2025-02-12 20:30:19 +0700

    /new lmm

--- src/content_option.js
+++ src/content_option.js
@@ -1,8 +1,9 @@
-const logotext = "Ridl";
+const logotext = "LMM";
 const meta = {
-  title: "Ridl",
+  title: "LMM",
   // description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
-  description: "Saya Rachmad Ridlo Baihaqi _ Backend Developer",
+  // description: "Saya Rachmad Ridlo Baihaqi _ Backend Developer",
+  description: "Kami Lembaga Mutiara Madani",
 };
 
 const introdata = {
@@ -13,13 +14,13 @@ const introdata = {
   //     third: "I develop mobile apps",
   // },
   // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-  title: "I'm Rachmad Ridlo",
+  title: "Lembaga Mutiara Madani",
   animated: {
     first: "I optimize server performance",
     second: "I build backend systems",
     third: "I ensure data security and integrity",
   },
-  description: "I'm Backend Developer",
+  description: "Lembaga Mutiara Madani",
 //   your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
   your_img_url: "https://images.unsplash.com/photo-1541560052-5e137f229371",
 };
@@ -27,19 +28,30 @@ const introdata = {
 const dataabout = {
   title: "About my self",
   aboutme:
-    "I am a graduate of Brawijaya University with a major in Computer Science, specializing in Machine Learning. I have a strong passion for developing intelligent applications utilizing Machine Learning technologies. My academic background and practical experience have equipped me with the skills to create predictive SP2D applications, implement search engines using Python, build company profiles with PHP, develop clinic information systems, and design laboratory information systems using JavaScript. I thrive in collaborative environments, possess excellent communication skills, and am always eager to learn and explore new technologies. With a deep commitment to continuous self-improvement and a desire to make a positive impact, I am enthusiastic about contributing to innovative projects and initiatives.",
+    // "I am a graduate of Brawijaya University with a major in Computer Science, specializing in Machine Learning. I have a strong passion for developing intelligent applications utilizing Machine Learning technologies. My academic background and practical experience have equipped me with the skills to create predictive SP2D applications, implement search engines using Python, build company profiles with PHP, develop clinic information systems, and design laboratory information systems using JavaScript. I thrive in collaborative environments, possess excellent communication skills, and am always eager to learn and explore new technologies. With a deep commitment to continuous self-improvement and a desire to make a positive impact, I am enthusiastic about contributing to innovative projects and initiatives.",
+    "lorem",
 };
 const worktimeline = [
   {
-    jobtitle: "Freelance",
+    jobtitle: "Lorem",
     where: "-",
     date: "2020",
   },
   {
-    jobtitle: "Backend Developer",
-    where: "PT WMI",
+    jobtitle: "Lorem",
+    where: "Lorem",
     date: "2021",
   },
+  // {
+  //   jobtitle: "Freelance",
+  //   where: "-",
+  //   date: "2020",
+  // },
+  // {
+  //   jobtitle: "Backend Developer",
+  //   where: "PT WMI",
+  //   date: "2021",
+  // },
   // {
   //   jobtitle: "Designer of week",
   //   where: "ALquds",
@@ -49,38 +61,43 @@ const worktimeline = [
 
 const skills = [
   {
-    name: "Python",
+    name: "Lorem",
     value: 80,
   },
   {
-    name: "Php",
+    name: "Lorem",
     value: 60,
   },
   {
-    name: "Javascript",
+    name: "Lorem",
     value: 90,
   },
   {
-    name: "Java",
+    name: "Lorem",
     value: 60,
   },
   {
-    name: "Go",
+    name: "Lorem",
     value: 60,
   },
 ];
 
 const services = [
   {
-    title: "Web Apps",
+    title: "Berita Terkini",
     description:
-      "Create REST API",
+      "membuat berita",
   },
-  {
-    title: "Mobile Apps",
-    description:
-      "Create REST API",
-  },
+  // {
+  //   title: "Web Apps",
+  //   description:
+  //     "Create REST API",
+  // },
+  // {
+  //   title: "Mobile Apps",
+  //   description:
+  //     "Create REST API",
+  // },
 //   {
 //     title: "Wordpress Design",
 //     description:
@@ -165,7 +182,8 @@ const dataportfolio = [
 ];
 
 const contactConfig = {
-  YOUR_EMAIL: "rahmadridho17@gmail.com",
+  // YOUR_EMAIL: "rahmadridho17@gmail.com",
+  YOUR_EMAIL: "lorem@gmail.com",
   YOUR_FONE: "(+62)123-4567",
   description:
     "Feel free to reach out to discuss potential collaborations or exciting opportunities in the tech world.",
