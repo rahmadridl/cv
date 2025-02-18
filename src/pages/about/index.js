bc94dcfb9a8b285df2bbbcf198b9b122016fa703
unknown <rahmadridho17@gmail.com>
2025-02-18 23:40:07 +0700

    /fix content again

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -75,7 +75,7 @@ export const About = () => {
             </table>
           </Col>
         </Row>
-        <Row className="sec_sp">
+        {/* <Row className="sec_sp">
           <Col lg="5">
             <h3 className="color_sec py-4">Skills</h3>
           </Col>
@@ -98,7 +98,7 @@ export const About = () => {
               );
             })}
           </Col>
-        </Row>
+        </Row> */}
         <Row className="sec_sp">
           <Col lang="5">
             <h3 className="color_sec py-4">services</h3>
